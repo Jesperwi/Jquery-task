11b3183dc8b4992f67393421e46334a249b3dd2a
Jesperwi <71511557+Jesperwi@users.noreply.github.com>
2020-10-21 15:24:02 +0200

    jquery task

---
+++ scripts.js
@@ -0,0 +1,34 @@
+function newItem(){
+
+  //javascript
+  //1. Adding a new item to the list of items: 
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+    
+  let list = $('#list');
+  if (inputValue === '') {
+   alert("You must write something!");
+   } else {
+    list.append(li);
+}
+    
+  //2. Crossing out an item from the list of items:
+  li.on("click", function(){
+    li.toggleClass("strike");
+});
+        
+    
+  //3(i). Adding the delete button "X": 
+  let crossOutButton = $('<crossOutbutton>x</crossOutButton>')
+    li.append(crossOutButton);
+  
+  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  crossOutButton.on('click', function(){
+    li.addClass("delete");
+});
+   // 4. Reordering the items: 
+  list.sortable();
+    
+}
+    
